aafbde35abbbb5a9eb7072883bba63308d8324b6
Tomasz Borychowski <tomasz.borychowski@sap.com>
2017-02-01 10:21:37 +0000

    changes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-/*global chrome */
+/* global chrome */
 (function () {
 	'use strict';
 
@@ -7,9 +7,11 @@
 	window.onload = function () {
 		frame = document.getElementById('frame');
 		chrome.tabs.getCurrent(function (tab) { currentTab = tab; });
+
+
 		chrome.storage.sync.get(null, function (cfg) {
 			if (cfg && cfg.url) {
-				if (cfg.url.indexOf('http://') === -1) cfg.url = 'http://' + cfg.url;
+				// if (cfg.url.indexOf('http://') === -1) cfg.url = 'http://' + cfg.url;
 				frame.src = cfg.url;
 			}
 			else chrome.tabs.update(currentTab.id, { url: 'settings.html' });
